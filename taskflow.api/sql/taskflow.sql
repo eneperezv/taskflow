76c744794b301493585346b233db313308bc5eb9
eneperezv <eneperezv@gmail.com>
2024-09-03 23:08:39 -0500

    Se incluye archivo de respaldo actualizado de la base de datos.
     Changes to be committed:
    	new file:   taskflow.api/sql/taskflow-2024-08-23.sql
    	modified:   taskflow.api/sql/taskflow.sql

--- taskflow.api/sql/taskflow.sql
+++ taskflow.api/sql/taskflow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-08-2024 a las 04:55:24
+-- Tiempo de generación: 04-09-2024 a las 06:07:56
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -26,6 +26,44 @@ USE `taskflow`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `dbo_tasks`
+--
+
+CREATE TABLE `dbo_tasks` (
+  `id_task` bigint(20) NOT NULL,
+  `date_create` varchar(10) DEFAULT NULL,
+  `date_expiration` varchar(10) DEFAULT NULL,
+  `description` varchar(1500) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `id_usuario` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `dbo_tasks`
+--
+
+INSERT INTO `dbo_tasks` (`id_task`, `date_create`, `date_expiration`, `description`, `name`, `status`, `id_usuario`) VALUES
+(1, '2024-09-03', '2024-10-03', 'Creacion de API SpringBoot para control y registro de tareas y seguimientod e las mismas', 'taskflow.api', '1', 1),
+(2, '2024-09-03', '2024-11-03', 'API SpringBoot para control de la aplicación web que permite a los usuarios reservar habitaciones de hotel, verificar disponibilidad y gestionar sus reservas.', 'reservite.api', '1', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dbo_task_followups`
+--
+
+CREATE TABLE `dbo_task_followups` (
+  `id_task_followup` bigint(20) NOT NULL,
+  `date_create` varchar(10) DEFAULT NULL,
+  `description` varchar(1500) DEFAULT NULL,
+  `id_task` bigint(20) NOT NULL,
+  `id_usuario` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `dbo_users`
 --
 
@@ -38,8 +76,30 @@ CREATE TABLE `dbo_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `dbo_users`
+--
+
+INSERT INTO `dbo_users` (`id_usuario`, `name`, `password`, `role`, `username`) VALUES
+(1, 'Eliezer Navarro Pérez', '$2a$10$DRtD6u4iWycTIrKGcrYuTOITp0JZq88lClhTIgzJ3YMvzlB7LnWOG', 'USER', 'enp');
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `dbo_tasks`
+--
+ALTER TABLE `dbo_tasks`
+  ADD PRIMARY KEY (`id_task`),
+  ADD KEY `FKau1nhjty23v4v8n2nkytfb647` (`id_usuario`);
+
+--
+-- Indices de la tabla `dbo_task_followups`
+--
+ALTER TABLE `dbo_task_followups`
+  ADD PRIMARY KEY (`id_task_followup`),
+  ADD KEY `FK473ommd02fmlwhih73tpv1ifn` (`id_task`),
+  ADD KEY `FK6e7yvxnhc1jsksecf3avp41eu` (`id_usuario`);
 
 --
 -- Indices de la tabla `dbo_users`
@@ -52,10 +112,39 @@ ALTER TABLE `dbo_users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `dbo_tasks`
+--
+ALTER TABLE `dbo_tasks`
+  MODIFY `id_task` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `dbo_task_followups`
+--
+ALTER TABLE `dbo_task_followups`
+  MODIFY `id_task_followup` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `dbo_users`
 --
 ALTER TABLE `dbo_users`
-  MODIFY `id_usuario` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `dbo_tasks`
+--
+ALTER TABLE `dbo_tasks`
+  ADD CONSTRAINT `FKau1nhjty23v4v8n2nkytfb647` FOREIGN KEY (`id_usuario`) REFERENCES `dbo_users` (`id_usuario`);
+
+--
+-- Filtros para la tabla `dbo_task_followups`
+--
+ALTER TABLE `dbo_task_followups`
+  ADD CONSTRAINT `FK473ommd02fmlwhih73tpv1ifn` FOREIGN KEY (`id_task`) REFERENCES `dbo_tasks` (`id_task`),
+  ADD CONSTRAINT `FK6e7yvxnhc1jsksecf3avp41eu` FOREIGN KEY (`id_usuario`) REFERENCES `dbo_users` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
