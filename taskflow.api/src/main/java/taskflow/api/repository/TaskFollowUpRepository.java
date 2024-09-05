ba6a7de4463328a744c38f7d700cd6927dd9a7e8
eneperezv <eneperezv@gmail.com>
2024-09-04 21:40:01 -0500

    Se incluye documentacion inicial de las nuevas clases Controller, Service y Repository para TaskFollowUp
     Changes to be committed:
    	modified:   taskflow.api/src/main/java/taskflow/api/controller/TaskFollowUpController.java
    	modified:   taskflow.api/src/main/java/taskflow/api/repository/TaskFollowUpRepository.java
    	modified:   taskflow.api/src/main/java/taskflow/api/service/TaskFollowUpService.java

--- taskflow.api/src/main/java/taskflow/api/repository/TaskFollowUpRepository.java
+++ taskflow.api/src/main/java/taskflow/api/repository/TaskFollowUpRepository.java
@@ -1,5 +1,22 @@
 package taskflow.api.repository;
 
+/*
+ * @(#)TaskFollowUpRepository.java 1.0 04/09/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Interface Repository para control de seguimiento de tareas y su persistencia JPA
+ *
+ * @author eliezer.navarro
+ * @version 1.0 | 04/09/2024
+ * @since 1.0
+ */
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import taskflow.api.entity.TaskFollowUp;
