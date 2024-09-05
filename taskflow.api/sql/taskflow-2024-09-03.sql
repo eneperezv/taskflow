9ef14a03318481ae7e5a450ac8c604cd06bb433a
eneperezv <eneperezv@gmail.com>
2024-09-04 21:34:54 -0500

    Se incluye archivo de respaldo de la base de datos.
     Changes to be committed:
    	new file:   taskflow.api/sql/taskflow-2024-09-03.sql
    	modified:   taskflow.api/sql/taskflow.sql

--- taskflow.api/sql/taskflow-2024-09-03.sql
+++ taskflow.api/sql/taskflow-2024-09-03.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-09-2024 a las 04:34:01
+-- Tiempo de generación: 04-09-2024 a las 06:07:56
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
